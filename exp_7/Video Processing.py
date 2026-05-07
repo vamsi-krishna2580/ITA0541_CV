e76e7c64bb72f73748e08fa57b26ac1018fbdffa
vamsi-krishna2580 <216156130+vamsi-krishna2580@users.noreply.github.com>
2026-05-07 13:07:16 +0530

    7 07-05-2026

--- exp_7/Video Processing.py
+++ exp_7/Video Processing.py
@@ -1,39 +1,29 @@
 import cv2
-import matplotlib.pyplot as plt
 
 cap = cv2.VideoCapture('video.mp4')
+speed = 1
 
-frames = []
-count = 0
-max_frames = 12 # Number of frames you want
-
-while cap.isOpened() and count < max_frames:
+while True:
     ret, frame = cap.read()
 
     if not ret:
         break
 
-    # Convert BGR to RGB
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    cv2.imshow("Video", frame)
 
-    frames.append(frame_rgb)
-    count += 1
+    key = cv2.waitKey(speed)
+
+    if key == ord('s'):
+        speed = 100   # Slow
+
+    elif key == ord('f'):
+        speed = 1     # Fast
+
+    elif key == ord('n'):
+        speed = 30    # Normal
+
+    elif key == ord('q'):
+        break
 
 cap.release()
-
-# Display frames
-cols = 4
-rows = (len(frames) + cols - 1) // cols
-
-plt.figure(figsize=(15, 8))
-
-for i in range(len(frames)):
-    plt.subplot(rows, cols, i + 1)
-    plt.imshow(frames[i])
-    plt.title(f"Frame {i+1}")
-    plt.axis('off')
-
-plt.tight_layout()
-plt.show()
-
-print(f"{len(frames)} frames displayed")
+cv2.destroyAllWindows()
